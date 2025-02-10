0feff03832e3dbee3d251f4ad04d9f7fbff8cd90
kashi992 <kashifrajput992@gmail.com>
2025-02-10 22:14:24 +0500

    email setup

---
+++ send-email.php
@@ -0,0 +1,25 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+
+    $to = "";
+    $subject = "SynergyLifeCare Contact Form";
+    $headers = "From: $name <$email>";
+
+    $email_content = "Name: $name\n";
+    $email_content .= "Email: $email\n\n";
+    $email_content .= "Message:\n$message\n";
+
+    if (mail($to, $subject, $email_content, $headers)) {
+        header('Location: /');
+        exit();
+    } else {
+        echo "Sorry, something went wrong. Please try again later.";
+    }
+
+} else {
+    echo "Sorry, this form cannot be submitted directly.";
+}
+?>
